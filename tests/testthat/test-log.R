34e1ecf4b3be28c9d4305048ab1de13978b413a6
majazaloznik <maja.zaloznik@gmail.com>
2022-06-29 10:13:36 +0200

    removed logging test cuz makes no sense

--- tests/testthat/test-log.R
+++ tests/testthat/test-log.R
@@ -1,4 +1,4 @@
-test_that("Test logging function", {
-  x <- suppressWarnings(log_script_run())
-  expect_equal(x, NA)
-})
+# test_that("Test logging function", {
+#   x <- suppressWarnings(log_script_run())
+#   expect_equal(x, NA)
+# })
