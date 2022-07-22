eb28ebb2ff7d95c42bca78cb5bad38560207b2bd
majazaloznik <maja.zaloznik@gmail.com>
2022-07-22 10:52:55 +0200

    Add missing tests for structure functions

--- tests/testthat/test-structure.R
+++ tests/testthat/test-structure.R
@@ -1,9 +1,9 @@
+rsp <- get_API_response()
+out <- parse_structAPI_response(rsp)
 test_that("check API response is appropriate and the parsing works", {
-  rsp <- get_API_response()
   expect_true(is.character(rsp))
-  expect_gte(nchar(rsp), 2000000)
-  out <- parse_structAPI_response(rsp)
   expect_true(is.environment(out))
+  expect_gte(nchar(rsp), 2000000)
   out <- parse_structAPI_response(rsp, output = "lol")
   expect_true(is.list(out))
   out <- parse_structAPI_response(rsp, output = "df")
@@ -12,4 +12,30 @@ test_that("check API response is appropriate and the parsing works", {
   expect_true(is.na(out))
 })
 
+test_that("node helper functions", {
+  expect_equal(node_name(out$`1`), "Socialna zaščita")
+  expect_equal(node_id(out$`1`), 45)
+  expect_equal(node_file_id(out$`1`$childPodrocja$`1`$matrixList$`1`),753518)
+  expect_true(grepl("letno",node_matrix_name(out$`1`$childPodrocja$`1`$matrixList$`1`)))
+  expect_true(grepl("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}$",
+                    node_updated(out$`1`$childPodrocja$`1`$matrixList$`1`)))
+  expect_equal(node_parent(out$`1`$childPodrocja), 45)
+})
+
+
+test_that("Structure is extracted properly", {
+  full <- get_full_structure(out)
+  expect_equal(ncol(full), 9)
+  expect_gt(nrow(full), 6000)
+  expect_true(all(sapply(full, \(x) !all(is.na(x)))))
+  mat_h <- get_matrix_hierarchy(full)
+  expect_true(nrow(mat_h) < nrow(full))
+  field_h <- get_field_hierarchy(full)
+  expect_true(nrow(field_h) < nrow(full))
+  expect_true(nrow(field_h) + nrow(mat_h) == nrow(full))
+  tree <- get_tree(mat_h)
+  expect_true(inherits(tree, "Node"))
+})
+
+rm(rsp, out)
 
