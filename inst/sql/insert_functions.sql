051f41c9ff79b8220d723dd029b88d3e6afd20e0
majazaloznik <maja.zaloznik@gmail.com>
2022-10-04 16:52:27 +0200

    Nicer SQL fun, that works but can't pass schema to

--- inst/sql/insert_functions.sql
+++ inst/sql/insert_functions.sql
@@ -7,13 +7,11 @@ CREATE OR REPLACE FUNCTION insert_new_table(code TEXT,
                                         source_id INTEGER,
                                         url TEXT,
                                         notes JSONB,
-                                        schema TEXT,
-                                        OUT table_id integer)
+                                        OUT table_id INTEGER)
 RETURNS INTEGER AS $$
 BEGIN
-    EXECUTE FORMAT('INSERT INTO %I.table (code, name, source_id, url, notes) 
+    INSERT INTO test_platform.table (code, name, source_id, url, notes)
     VALUES (code, name, source_id, url, notes)
-    RETURNING id INTO table_id;', schema);
+    RETURNING id   INTO table_id;
 END;
 $$ LANGUAGE plpgsql;
-
